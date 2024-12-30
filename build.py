32459d065689206e5ec187a1ede2ca7612da1c70
Arcadiy Ivanov <arcadiy@ivanov.biz>
2024-12-30 09:12:50 -0500

    Start 0.0.10.dev [skip ci]

--- build.py
+++ build.py
@@ -25,7 +25,7 @@ use_plugin("python.coveralls")
 use_plugin("copy_resources")
 
 name = "geventmp"
-version = "0.0.9"
+version = "0.0.10.dev"
 
 summary = "Multiprocessing Gevent Extension"
 authors = [Author("Karellen, Inc.", "supervisor@karellen.co")]
